4c1b887c6b85d22377a08837d8d32566bffb48c1
thislock <averypunnyskeletonn@gmail.com>
2025-09-13 12:58:23 -0400

    raw brah

--- trick/display/src/lib.rs
+++ trick/display/src/lib.rs
@@ -3,6 +3,8 @@ pub fn something() {
   println!("hello from display!")
 }
 
+
+
 trait DisplayHandle {
-  fn get_raw_handle(&self);
+  fn get_raw_handle(&self) -> anyhow::Result<raw_window_handle::WindowHandle<'_>>;
 }
